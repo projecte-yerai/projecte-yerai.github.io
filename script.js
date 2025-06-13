ed0f4b58bc2f58fe54a9111db8dab74b587b5ef2
projecte-yerai <jvidal99@xtec.cat>
2025-06-13 11:34:42 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const appsScriptUrl = "https://script.google.com/macros/s/AKfycb.../exec"; // la teva URL
 
+function actualitzarDades() {
+  fetch(appsScriptUrl)
+    .then(res => res.json())
+    .then(data => {
+      document.getElementById("temp").innerText = data.temperatura;
+      document.getElementById("llum").innerText = data.llum;
+    })
+    .catch(err => console.error("Error en rebre dades de la Sheet:", err));
+}
